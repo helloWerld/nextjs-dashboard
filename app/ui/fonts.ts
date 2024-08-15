102d2a76b76c47b3af510ae296a670ee00cf466d
helloWerld <108149462+helloWerld@users.noreply.github.com>
2024-08-15 15:58:13 -0400

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Playpen_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+export const playpen = Playpen_Sans({ subsets: ['latin'], weight: '700' });
